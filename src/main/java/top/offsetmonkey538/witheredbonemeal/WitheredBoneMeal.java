e1e30122d3bf6694641eb7aa7495af845d54c2bd
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2023-03-14 19:27:18 +0200

    Make wither skeletons drop withered bones

--- src/main/java/top/offsetmonkey538/witheredbonemeal/WitheredBoneMeal.java
+++ src/main/java/top/offsetmonkey538/witheredbonemeal/WitheredBoneMeal.java
@@ -1,6 +1,11 @@
 package top.offsetmonkey538.witheredbonemeal;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
+import net.minecraft.entity.EntityType;
+import net.minecraft.loot.LootPool;
+import net.minecraft.loot.entry.ItemEntry;
+import net.minecraft.loot.provider.number.UniformLootNumberProvider;
 import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,6 +20,11 @@ public class WitheredBoneMeal implements ModInitializer {
 	public void onInitialize() {
 		ModBlocks.initialize();
 		ModItems.initialize();
+
+		LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
+			if (!EntityType.WITHER_SKELETON.getLootTableId().equals(id) || !source.isBuiltin()) return;
+			tableBuilder.pool(LootPool.builder().with(ItemEntry.builder(ModItems.WITHERED_BONE)).rolls(UniformLootNumberProvider.create(1, 3)));
+		});
 	}
 
 	public static Identifier id(String name) {
