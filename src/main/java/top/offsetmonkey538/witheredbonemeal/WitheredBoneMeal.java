e0b1146c3a6b90bee07875e49e37f640ba9cbc3a
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2023-03-14 17:35:55 +0200

    Initial Commit

---
+++ src/main/java/top/offsetmonkey538/witheredbonemeal/WitheredBoneMeal.java
@@ -0,0 +1,15 @@
+package top.offsetmonkey538.witheredbonemeal;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WitheredBoneMeal implements ModInitializer {
+	public static final String MOD_ID = "withered-bone-meal";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/top/offsetmonkey538/witheredbonemeal/datagen/WitheredBoneMealDatagen.java
@@ -0,0 +1,14 @@
+package top.offsetmonkey538.witheredbonemeal.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class WitheredBoneMealDatagen implements DataGeneratorEntrypoint {
+
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+        FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+
+
+    }
+}
