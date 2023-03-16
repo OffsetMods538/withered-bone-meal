dac257c2dda6219006e915fb6707c4b616781eb8
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2023-03-16 17:05:08 +0200

    Fix withered bone meal giving 4 black dye instead of 1

--- src/main/java/top/offsetmonkey538/witheredbonemeal/datagen/ModRecipeProvider.java
+++ src/main/java/top/offsetmonkey538/witheredbonemeal/datagen/ModRecipeProvider.java
@@ -19,6 +19,6 @@ public class ModRecipeProvider extends FabricRecipeProvider {
     public void generate(Consumer<RecipeJsonProvider> exporter) {
         offerReversibleCompactingRecipesWithReverseRecipeGroup(exporter, RecipeCategory.MISC, ModItems.WITHERED_BONE_MEAL, RecipeCategory.BUILDING_BLOCKS, ModItems.WITHERED_BONE_BLOCK, "withered_bone_meal_from_withered_bone_block", "withered_bone_meal");
         ShapelessRecipeJsonBuilder.create(RecipeCategory.MISC, ModItems.WITHERED_BONE_MEAL, 3).input(ModItems.WITHERED_BONE).group("withered_bone_meal").criterion("has_withered_bone", conditionsFromItem(ModItems.WITHERED_BONE)).offerTo(exporter, "withered_bone_meal_from_withered_bone");
-        ShapelessRecipeJsonBuilder.create(RecipeCategory.MISC, Items.BLACK_DYE, 4).input(ModItems.WITHERED_BONE_MEAL).group("black_dye").criterion("has_withered_bone_meal", conditionsFromItem(ModItems.WITHERED_BONE_MEAL)).offerTo(exporter, "black_dye_from_withered_bone_meal");
+        ShapelessRecipeJsonBuilder.create(RecipeCategory.MISC, Items.BLACK_DYE).input(ModItems.WITHERED_BONE_MEAL).group("black_dye").criterion("has_withered_bone_meal", conditionsFromItem(ModItems.WITHERED_BONE_MEAL)).offerTo(exporter, "black_dye_from_withered_bone_meal");
     }
 }
